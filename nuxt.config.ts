733f7abf7ad670a36a35ea603f0d577f4fb9bf56
Kevin Tan <cekevintan@gmail.com>
2025-04-26 21:13:17 +0800

    added shad cn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image'
-  ]
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    modules: [
+        '@nuxt/eslint',
+        '@nuxt/fonts',
+        '@nuxt/icon',
+        '@nuxt/image',
+        'shadcn-nuxt',
+    ],
+    css: ['~/assets/css/tailwind.css'],
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: '',
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: './components/ui'
+    }
 })
